7a64a14a4d7aa6cf5128c965e6155af2ee91a559
Chathuka-Pehesara <chathukapehesara02@gmail.com>
2026-02-21 15:01:06 +0530

    Created Inventory Entity class.

---
+++ Tea-Planter-BackEnd/src/main/java/online/teaplanter/teaplanterbackend/entity/InventoryItem.java
@@ -0,0 +1,25 @@
+package online.teaplanter.teaplanterbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "inventory_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InventoryItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String category;
+    private double currentStock;
+    private double unitPrice;
+    private String unit;
+    private double recorderLevel;
+}
